74b82f460dd7548d2b5c9e1a2de8b92063e8c931
Magnus Sagerup <magnus.sagerup@gmail.com>
2018-05-14 22:11:35 +0200

    used a swtich to display different feedback from picked weapons

--- src/components/weaponsP2.js
+++ src/components/weaponsP2.js
@@ -1,14 +1,28 @@
-import React from "react";
-import { Button } from "semantic-ui-react";
+import React, { Component } from "react";
 
-function WeaponsP2(props) {
-  return (
-    <div>
-      <p>You pick up the {props.selectedWeapon.name}</p>
-      It has a hitpoints of : {props.selectedWeapon.hitPoints}
-      and a defence of: {props.selectedWeapon.defence}
-    </div>
-  );
+export default class WeaponsP2 extends Component {
+  constructor(props) {
+    super(props);
+    this.weaponSelected = this.weaponSelected.bind(this);
+  }
+
+  weaponSelected(weapon) {
+    switch (weapon.name) {
+      case "The Shield Meidens Sword of Death and Shield of Doom":
+        return (
+          <div>
+            As you walk over to the table you select the Sword and Shield, its
+            name is {weapon.name}.
+            <ul>
+              <li>It has a damage of {weapon.hitPoints}</li>
+              <li>It has a defence of {weapon.defence}</li>
+            </ul>
+          </div>
+        );
+    }
+  }
+
+  render() {
+    return <div>{this.weaponSelected(this.props.selectedWeapon)}</div>;
+  }
 }
-
-export default WeaponsP2;
